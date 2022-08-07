0bd00b17d776051e1c455a981dc79e5d62ab2033
Rezaul909 <reza1381996@gmail.com>
2022-08-08 01:12:20 +0600

    header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import logo from '../../images/Logo.svg'
+import './Header.css';
 
 const Header = () => {
     return (
         <div>
-            
+            <nav className='header-nav'>
+                <img src={logo} alt="" />
+                <div>
+                    <a href="/Home">Home</a>
+                    <a href="/Shop">Shop</a>
+                    <a href="/Order Review">Order Review</a>
+                    <a href="/Manage Inventory">Manage Inventory</a>
+                    <a href="/Sign-Up">Sign-Up</a>
+                </div>
+            </nav>
         </div>
     );
 };
