8572725f4a1fb0729b457e0c8bf63980cfe24986
Rezaul909 <reza1381996@gmail.com>
2022-08-12 18:20:34 +0600

    cart calculation complete

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,15 +2,26 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = ({cart}) => {
+
+    let total = 0;
+    let totalShippingCost = 0;
+    for(const product of cart){
+        total = total + product.price;
+        totalShippingCost = totalShippingCost + product.shipping;
+       
+    }
+    const tax = (total*5)/100;
+    let grandTotal = total + totalShippingCost + tax;
+
     return (
         <div className='cart'>
             <h3 className='text-center pt-5'>Order summery</h3>
             <div className="ms-4">
                 <p>Selected Items : {cart.length} </p>
-                <p>Total Price : $ </p>
-                <p>Total Shipping Charge : $ </p>
-                <p>Tax : $ </p>
-                <h3>Grand Total: $ </h3>
+                <p>Total Price : ${total} </p>
+                <p>Total Shipping Charge : ${totalShippingCost} </p>
+                <p>Tax : ${tax} </p>
+                <h5>Grand Total: ${grandTotal} </h5>
             </div>
         </div>
     );
