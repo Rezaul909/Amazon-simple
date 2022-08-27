f66a98fa3450e1d9c053f17d343fd6f23a4a56fe
Rezaul909 <reza1381996@gmail.com>
2022-08-27 12:35:13 +0600

    SignUp-form-added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
-import OrderReview from './components/OrderReview/OrderReview';
-import Shop from './components/Shop/Shop';
-import SignUp from './components/SignUp/SignUp';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import OrderReview from "./components/OrderReview/OrderReview";
+import Shop from "./components/Shop/Shop";
+import SignUp from "./components/SignUp/SignUp";
 
 function App() {
   return (
-    <div >
+    <div>
       <Header></Header>
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/Home' element={<Home></Home>}></Route>
-        <Route path='/Shop' element={<Shop></Shop>}></Route>
-        <Route path='/OrderReview' element={<OrderReview></OrderReview>}></Route>
-        <Route path='/SignUp' element={<SignUp></SignUp>}></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/Home" element={<Home></Home>}></Route>
+        <Route path="/Shop" element={<Shop></Shop>}></Route>
+        <Route
+          path="/OrderReview"
+          element={<OrderReview></OrderReview>}
+        ></Route>
+        <Route path="/SignUp" element={<SignUp></SignUp>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
