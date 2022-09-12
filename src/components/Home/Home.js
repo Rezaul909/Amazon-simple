907a567b3a327d8117bb1c17e7d9d9ae9e02b73f
Rezaul909 <reza1381996@gmail.com>
2022-09-12 11:32:53 +0600

    firebase-deployed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Home.css'
 import banner from '../../images/HomeMe.jpg';
 import PopularProducts from '../PopularProducts/PopularProducts';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
@@ -11,7 +12,9 @@ const Home = () => {
                     <h4 className='text-warning'>Sale Up to 70% off!</h4>
                     <h1 className='mt-5'>New Collection For Fall</h1>
                     <p>Discover all the new arrivals of ready-to-wear collection.</p>
-                    <button className='home-btn'>SHOP NOW</button>
+                    <Link to="/Shop">
+                        <button className='home-btn'>SHOP NOW</button>
+                    </Link>
                 </div>
                 <div className="home-banner">
                     <img src={banner} alt="" />
